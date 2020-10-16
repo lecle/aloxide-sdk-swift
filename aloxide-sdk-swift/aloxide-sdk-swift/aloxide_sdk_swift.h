f9200d4b11832349f1cbff9eab3bcc55d1dfcc70
quoc huynh <quochuynh@lecle.co.kr>
2020-10-16 15:12:02 +0700

    add swift code

---
+++ aloxide-sdk-swift/aloxide-sdk-swift/aloxide_sdk_swift.h
@@ -0,0 +1,18 @@
+//
+//  aloxide_sdk_swift.h
+//  aloxide-sdk-swift
+//
+//  Created by quoc huynh on 10/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for aloxide_sdk_swift.
+FOUNDATION_EXPORT double aloxide_sdk_swiftVersionNumber;
+
+//! Project version string for aloxide_sdk_swift.
+FOUNDATION_EXPORT const unsigned char aloxide_sdk_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aloxide_sdk_swift/PublicHeader.h>
+
+
